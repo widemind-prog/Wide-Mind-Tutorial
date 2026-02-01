63fccefd7d4fddcbbe7e840895cd211a95899281
Wide Mind <wideminddevs@gmail.com>
2026-02-01 12:04:16 +0100

    WMT V.1.0.0

--- static/js/auth.js
+++ static/js/auth.js
@@ -25,21 +25,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-
-<script>
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleIcon = document.querySelector(".toggle-password-icon");
-    const passwordInput = document.getElementById("password");
-
-    toggleIcon.addEventListener("click", () => {
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-            // Optional: swap icon to an "eye-open" variant
-            toggleIcon.src = "/static/icons/eye-skeleton-open.png";
-        } else {
-            passwordInput.type = "password";
-            toggleIcon.src = "/static/icons/eye-skeleton.png";
-        }
-    });
-});
-</script>
