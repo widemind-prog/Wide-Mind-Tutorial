489cdd6b3ede4d858aed98b70a5dec1db6f91808
Wide Mind <wideminddevs@gmail.com>
2026-03-09 12:11:41 +0100

    WMT V.1.0

--- extensions.py
+++ extensions.py
@@ -1,3 +1,3 @@
 from flask_socketio import SocketIO, emit, join_room
 
-socketio = SocketIO(cors_allowed_origins="*", async_mode="threading", logger=True, engineio_logger=True)
+socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet", logger=True, engineio_logger=True)
