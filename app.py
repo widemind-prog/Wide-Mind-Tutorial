9f119d11599be70adc6e809f503a3cb453cea457
Wide Mind <wideminddevs@gmail.com>
2026-03-28 07:09:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ def register():
     if not c.fetchone():
         c.execute(
             "INSERT INTO payments (user_id, amount, status) VALUES (?, ?, ?)",
-            (user_id, 1000000, "unpaid")
+            (user_id, 1024632, "unpaid")
         )
 
     conn.commit()
